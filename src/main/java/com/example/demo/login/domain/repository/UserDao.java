4168d3522913c1c5fe2b746de62275359873af55
itoshuhei <ito.shuhei@tis.co.jp>
2021-01-01 23:04:48 +0900

    リポジトリークラスを新規作成

---
+++ src/main/java/com/example/demo/login/domain/repository/UserDao.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import org.springframework.dao.DataAccessException;
+import com.example.demo.login.domain.model.User;
+
+public interfase UserDao{
+
+   public int count() throws DataAccessException;
+
+   public int insertOne(User user) throws DataAccessException;
+
+   public User selectOne(String userId) throws DataAccessEception;
+
+   public List<User> selectMany() throws DataAccessException;
+
+   public int updateOne(User user) throws DataAccessException;
+
+   public int deleteOne(String userId) throws DataAccessException;
+
+   public void userCsvOut() throws DataAccessException;
+
+}
