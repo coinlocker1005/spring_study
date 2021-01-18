1f1da7b65cc43716293ba5b384e73b0090ff1535
itoshuhei <ito.shuhei@tis.co.jp>
2021-01-18 23:43:36 +0900

    コントローラーとhtmlを修正

--- src/main/java/com/example/demo/trySpring/HelloController.java
+++ src/main/java/com/example/demo/trySpring/HelloController.java
@@ -2,6 +2,10 @@ package com.example.demo.trySpring;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class HelloController {
@@ -10,4 +14,11 @@ public class HelloController {
     public String getHello(){
         return "hello";
     }
+
+    @PostMapping("/hello")
+    public String postRequest(@RequestParam("text1")String str, Model model){
+        model.addAttribute("sample", str);
+        return "helloResponse";
+    }
 }
+
