c8a348e5451dc7aa2deaea02b7a46d079034a1ed
itoshuhei <ito.shuhei@tis.co.jp>
2021-01-19 22:24:49 +0900

    sqlファイルとpropertiesファイルを更新

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO dmployee(employee_id, employee_name, age)
+VALUES(1, '山田太郎', 30);
---
+++ src/main/resources/shema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS employee(
+  employee_id INT PRIMARY KEY,
+  EMPLOYEE_NAME VARCHAR(50),
+  age INT
+  );
