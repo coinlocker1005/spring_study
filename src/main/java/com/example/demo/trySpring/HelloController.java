63c5afdff9a64c86b069d811f335b49c9b9fe47c
itoshuhei <ito.shuhei@tis.co.jp>
2021-01-21 00:20:38 +0900

    リポジトリ、コントローラー、サービスを新規追加

--- src/main/java/com/example/demo/trySpring/HelloController.java
+++ src/main/java/com/example/demo/trySpring/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.trySpring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
+    @Autowired
+    private HelloService helloService;
+
     @GetMapping("/hello")
     public String getHello(){
         return "hello";
@@ -19,6 +23,19 @@ public class HelloController {
     public String postRequest(@RequestParam("text1")String str, Model model){
         model.addAttribute("sample", str);
         return "helloResponse";
+    }
+
+    @PostMapping("/hello/db")
+    public String postDbRequest(@RequestParam("text2")String str, Model model){
+
+        int id = Integer.parseInt(str);
+
+        Employee employee = helloService.findOne(id);
+        model.addAttribute("id", employee.getEmployeeId());
+        model.addAttribute("name", employee.getEmployeeName());
+        model.addAttribute("age", employee.getAge());
+
+        return "helloResponseDB";
     }
 }
 
